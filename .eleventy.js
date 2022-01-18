54c1209e7199b3636b99fc9eb3557f95fb6f3851
perezre <rebecaperez@outlook.de>
2022-01-18 16:48:16 +0100

    favicon

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
   eleventyConfig.addPassthroughCopy('./src/img');
+  eleventyConfig.addPassthroughCopy({ "./src/favicon": "/" });
   return {
     dir: {
       input: 'src',
